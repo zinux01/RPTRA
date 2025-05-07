f88554096369b74720fc0ee13dc41634f65af21a
Detection <studioaster23@gmail.com>
2025-05-07 22:46:48 +0700

    blablabla

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Contoh sederhana untuk scroll halus
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
